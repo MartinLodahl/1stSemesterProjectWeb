8aa4f3f157d78b73932131ceff4b5b05ec9ef4ed
MartinLodahl <MartinLodahl@10.50.137.56>
2017-01-11 10:31:11 +0100

    Merge origin/master
    Conflicts:
    	src/turnament/SortTurnament.java

---
+++ SemesterAfslutningWebApp/src/java/semesterafslutning/ICharacter.java
@@ -0,0 +1,29 @@
+package semesterafslutning;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Pravien
+ */
+public interface ICharacter
+{
+     public String getName();
+    
+
+    public void setName(String name);
+    
+
+    public int getHealth();
+    
+
+    public void setHealth(int health);
+    
+   
+    
+} 
