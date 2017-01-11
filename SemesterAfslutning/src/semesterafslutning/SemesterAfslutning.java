7ab7d22339e3099208c920ba277218d5e4f51e3c
MartinLodahl <MartinLodahl@10.50.137.56>
2017-01-11 10:08:39 +0100

    Merge origin/master
    Conflicts:
    	src/turnament/SortTurnament.java

--- SemesterAfslutning/src/semesterafslutning/SemesterAfslutning.java
+++ SemesterAfslutning/src/semesterafslutning/SemesterAfslutning.java
@@ -18,4 +18,23 @@ public class SemesterAfslutning {
         // TODO code application logic here
     }
     
+    /*
+     DATABASE TABLE SCRIPT
+    
+ DROP TABLE IF exists room;
+ DROP TABLE IF exists link;
+ 
+ CREATE TABLE room
+ (ID int(11),
+ Description text);
+ 
+
+ 
+ CREATE TABLE link 
+ (room_id int(11),
+ direction varchar(255),
+ goto int(11));
+ 
+    */
+    
 }
