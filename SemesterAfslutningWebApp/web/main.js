039aa866a116734feb60bee870e66f9f6787d76c
Jakob Varmose Bentzen <jakobvarmose>
2017-01-12 11:05:29 +0100

    a

--- SemesterAfslutningWebApp/web/main.js
+++ SemesterAfslutningWebApp/web/main.js
@@ -8,14 +8,10 @@ var west = document.getElementById('west')
 var currentRoom = 0
 
 function goTo(direction) {
-    var data = new FormData()
-    data.append('room', String(currentRoom))
-    data.append('direction', direction)
-
     var xhr = new XMLHttpRequest()
     xhr.open('POST', 'Gameserv', true)
-    xhr.send('room='+currentRoom+'&direction='+direction)
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send('room='+currentRoom+'&direction='+direction)
 
     xhr.onload = function() {
         try {
