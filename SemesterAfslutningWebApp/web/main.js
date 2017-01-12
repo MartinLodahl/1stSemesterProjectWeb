c210c2505dfb8d6fb8d51127d91bb7587729124e
Jakob Varmose Bentzen <jakobvarmose>
2017-01-12 14:02:49 +0100

    a

--- SemesterAfslutningWebApp/web/main.js
+++ SemesterAfslutningWebApp/web/main.js
@@ -18,6 +18,50 @@ function startGame() {
     west.style.display = 'none'
 }
 
+function addItem(item) {
+    var img = new Image()
+    img.src = item.picture
+    img.style.position = 'absolute'
+    img.style.left = item.x+'px'
+    img.style.top = item.y+'px'
+    img.onclick = function() {
+        pickUp(item.id)
+        items.removeChild(img)
+    }
+    items.appendChild(img)
+}
+
+function show(obj) {
+    start.style.display = 'none'
+    if (obj.north) {
+        north.style.display = 'block'
+    } else {
+        north.style.display = 'none'
+    }
+    if (obj.south) {
+        south.style.display = 'block'
+    } else {
+        south.style.display = 'none'
+    }
+    if (obj.east) {
+        east.style.display = 'block'
+    } else {
+        east.style.display = 'none'
+    }
+    if (obj.west) {
+        west.style.display = 'block'
+    } else {
+        west.style.display = 'none'
+    }
+    game.style.backgroundImage='url("'+obj.picture+'")'
+    currentRoom = obj.room
+    playerId = obj.playerId
+    for (var i = 0; i < obj.items.length; i++) {
+        var item = obj.items[i]
+        addItem(item)
+    }
+}
+
 function goTo(direction) {
     var xhr = new XMLHttpRequest()
     xhr.open('POST', 'Gameserv', true)
@@ -27,39 +71,7 @@ function goTo(direction) {
     xhr.onload = function() {
         try {
             var obj = JSON.parse(xhr.responseText)
-            start.style.display = 'none'
-            if (obj.north) {
-                north.style.display = 'block'
-            } else {
-                north.style.display = 'none'
-            }
-            if (obj.south) {
-                south.style.display = 'block'
-            } else {
-                south.style.display = 'none'
-            }
-            if (obj.east) {
-                east.style.display = 'block'
-            } else {
-                east.style.display = 'none'
-            }
-            if (obj.west) {
-                west.style.display = 'block'
-            } else {
-                west.style.display = 'none'
-            }
-            game.style.backgroundImage='url("'+obj.picture+'")'
-            currentRoom = obj.room
-            playerId = obj.playerId
-            for (var i = 0; i < obj.items.length; i++) {
-                var item = obj.items[i]
-                var img = new Image()
-                img.src = item.picture
-                img.style.position = 'absolute'
-                img.style.left = item.x+'px'
-                img.style.top = item.y+'px'
-                items.appendChild(img)
-            }
+            show(obj)
         } catch (ex) {
             alert(xhr.responseText)
         }
@@ -70,10 +82,15 @@ function pickUp(id) {
     var xhr = new XMLHttpRequest()
     xhr.open('POST', 'PickUp', true)
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send('room='+currentRoom+'&direction='+direction+'&playerId='+playerId)
+    xhr.send('room='+currentRoom+'&direction=PICK_UP&playerId='+playerId+'&itemId='+id)
 
     xhr.onload = function() {
-        
+        try {
+            var obj = JSON.parse(xhr.responseText)
+            show(obj)
+        } catch (ex) {
+            alert(xhr.responseText)
+        }
     }
 }
 
