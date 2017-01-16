089e008b5b3318b37158f7e85d5b2e54bd34b0f4
Jakob Varmose Bentzen <jakobvarmose>
2017-01-16 09:11:08 +0100

    1

--- SemesterAfslutningWebApp/dungeononlineMere.sql
+++ SemesterAfslutningWebApp/dungeononlineMere.sql
@@ -33,31 +33,32 @@
  
  CREATE TABLE itemtypes
  (type int,
- picName VARCHAR(40),
+ picture VARCHAR(40),
  stat double,
  modify int,
  note varchar(200));
 
  CREATE TABLE monster
  (ID int (11) UNIQUE,
+ picture varchar(40),
  description text,
  health int(11),
  attack int(11));
  
  INSERT INTO monster
  values 
- (1, 'This is a mean alf', 20, 5),
- (2, 'This is a mean dog', 30, 5),
- (3, 'This is the Flying DutchMan', 50, 2),
- (4, 'This person.. Is it a person? KILL IT', 10, 2);
+ (1, '', 'This is a mean alf', 20, 5),
+ (2, '', 'This is a mean dog', 30, 5),
+ (3, '', 'This is the Flying DutchMan', 50, 2),
+ (4, '', 'This person.. Is it a person? KILL IT', 10, 2);
  
  Insert into itemtypes
  values
- (1,'coin',1,50,'Conin give you money'),
- (2,'Kindregret',2,20,'Armor of type Kindregret, give u bonus health'),
- (3,'Manticore',3,10,'Ranged weapon type Manticore, give u bonus attack dmg'),
- (4,'Bonewrath',3,15,'Melee weapon type Bonewrath, give u bonus attack dmg'),
- (5,'Dishonored',3,25,'Melle weapon type Dishonored, give u bonus attack dmg');
+ (1,'coins',1,50,'Conin give you money'),
+ (2,'Armor/Kindregret',2,20,'Armor of type Kindregret, give u bonus health'),
+ (3,'WeaponsRanged/Manticore',3,10,'Ranged weapon type Manticore, give u bonus attack dmg'),
+ (4,'WeaponsMelee/Bonewrath',3,15,'Melee weapon type Bonewrath, give u bonus attack dmg'),
+ (5,'WeaponsMelee/Dishonored',3,25,'Melle weapon type Dishonored, give u bonus attack dmg');
  
  INSERT INTO items
  VALUES
