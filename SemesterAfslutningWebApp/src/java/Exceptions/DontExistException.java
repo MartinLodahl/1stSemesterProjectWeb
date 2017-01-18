195089648d02af3968e4afc74485960d88c6ee62
Jakob Varmose Bentzen <jakobvarmose>
2017-01-18 13:06:35 +0100

    1

--- SemesterAfslutningWebApp/src/java/Exceptions/DontExistException.java
+++ SemesterAfslutningWebApp/src/java/Exceptions/DontExistException.java
@@ -1,28 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exceptions;
 
-/**
- *
- * @author Pravien
- */
 public class DontExistException extends Exception
 {
-     private String type;
-     private int id;
+     private final String type;
+     private final int id;
 
     public DontExistException(int id, String type)
     {
        this.id=id;
        this.type=type;
-    }
-
-    DontExistException()
-    {
-        this.toString();
     }
 
     @Override
